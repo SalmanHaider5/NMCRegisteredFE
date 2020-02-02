600641958386e4fa36cf7fd0ca52bf7a027109cc
Salman Haider <salman.hadi98@gmail.com>
2020-02-02 22:07:59 +0500

    Code for Mal

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'https://intelligent-innovation.herokuapp.com/'
+// export const SERVER_URL = 'https://intelligent-innovation.herokuapp.com/'
+export const SERVER_URL = 'http://localhost:1000/'
