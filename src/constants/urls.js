72e909aed848f356fa5ca465982449699d4780b5
Salman Haider <salman.hayder112@gmail.com>
2021-02-24 08:35:27 +0500

    Final

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
-// export const SERVER_URL      = 'http://localhost:1000'
-export const SERVER_URL = 'https://server.nmcregistered.org.uk'
-export const DOCUMENTS_URL = 'https://server.nmcregistered.org.uk/uploads/'
+export const SERVER_URL      = 'http://78.129.165.44:1000'
+// export const SERVER_URL = 'https://server.nmcregistered.org.uk'
+export const DOCUMENTS_URL = 'http://78.129.165.44:1000/uploads/'
 export const GET_ADDRESS_URL = 'https://api.getAddress.io/'
